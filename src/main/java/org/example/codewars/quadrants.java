2aea724b848f1278e96b0fceaaff48c1e1d5c47a
wgnico <nicoburgos1989@gmail.com>
2025-11-04 17:42:04 -0300

    se agrega ejercicios de codewars

---
+++ src/main/java/org/example/codewars/quadrants.java
@@ -0,0 +1,23 @@
+package org.example.codewars;
+
+public class quadrants {
+//    Task
+//    Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+//
+//    Examples
+//            (1, 2)     => 1
+//            (3, 5)     => 1
+//            (-10, 100) => 2
+//            (-1, -9)   => 3
+//            (19, -56)  => 4
+
+    public static int quadrant(int x, int y) {
+        if (x > 0 && y > 0)
+            return 1;
+        else if( x < 0 &&  y > 0)
+            return 2;
+        else if( x > 0 &&  y < 0)
+            return 4;
+        else return 3;
+    }
+}
